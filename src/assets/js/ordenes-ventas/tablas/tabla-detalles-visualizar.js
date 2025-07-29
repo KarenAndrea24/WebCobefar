407f344f953e7f59f1272a2bb73f94a4025a6634
KarenAndrea24 <134449861+KarenAndrea24@users.noreply.github.com>
2025-07-29 00:17:35 -0500

    Cambios

---
+++ src/assets/js/ordenes-ventas/tablas/tabla-detalles-visualizar.js
@@ -0,0 +1,24 @@
+'use strict';
+$(function () {
+  $('#tabla-detalles-visualizar').DataTable({
+    paging     : true,
+    searching  : true,
+    info       : true,
+    ordering   : true,
+    displayLength: 5,
+    lengthMenu: [5],
+    lengthChange: true,
+    scrollX    : true,
+    autoWidth  : false,
+    language   : datatableLang(),
+  });
+});
+
+function datatableLang () {
+  return {
+    paginate: {
+      next: '<i class="ti ti-chevron-right ti-sm"></i>',
+      previous: '<i class="ti ti-chevron-left ti-sm"></i>'
+    }
+  };
+}
